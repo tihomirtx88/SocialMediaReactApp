63bc5d771a66f078915aac945ef312b26cd6626c
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-11-09 21:04:27 +0200

    Create profile desing and template

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,11 +1,72 @@
-import "./Profile.scss"
+import "./Profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 
 const Profile = () => {
-   
-
-    return(
-        <div className="profile">Profile</div>
-    )
-}
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://atd-blog.s3.us-east-2.amazonaws.com/wp-content/uploads/2022/04/16142811/cool-profile-pictures-for-tiktok-5-678x1024.webp"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://learn.microsoft.com/answers/storage/attachments/209536-360-f-364211147-1qglvxv1tcq0ohz3fawufrtonzz8nq3e.jpg"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+          <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>tihomir.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts/>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
